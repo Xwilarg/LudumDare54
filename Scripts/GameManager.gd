d103e37550948546581994abaed9cef384ba8766
Xwilarg <xwilarg@protonmail.com>
2023-09-30 12:31:51 +0200

    Add raycast

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -1,9 +1,11 @@
-extends Node
+extends Node3D
 
 var _buttons: Array[ItemButton]
 @export var _deck: JSON
 
 var _cards: Array[Card]
+
+var _selected_item: Card
 
 func _init_cards():
 	_cards.append(Card.new(_deck.data[0]))
@@ -15,4 +17,17 @@ func subscribe_button(b: ItemButton) -> void:
 	_buttons.append(b)
 
 func load_item(item: Card) -> void:
-	print(item.name)
+	_selected_item = item
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
+		var camera3d = get_node("/root/Root/Camera3D")
+		var space_state = get_world_3d().direct_space_state
+		var mousepos = event.position
+
+		var from = camera3d.project_ray_origin(event.position)
+		var to = from + camera3d.project_ray_normal(event.position) * 100000.0
+		var query = PhysicsRayQueryParameters3D.create(from, to)
+
+		var result = space_state.intersect_ray(query)
+		print(len(result))
