e48695324b733419e902d7657389fb030e83270a
Xwilarg <xwilarg@protonmail.com>
2023-09-30 18:32:12 +0200

    Load translations properly

--- Scripts/Deck/Card.gd
+++ Scripts/Deck/Card.gd
@@ -1,11 +1,14 @@
 class_name Card
 
 func _init(json: Dictionary):
-	name = json.name
+	name = tr(json.name + "_NAME")
+	description = tr(json.name + "_DESC")
+	
 	effects = json.effects
 	if json.model == "size2Guns": preload("res://Models/size2Guns.glb")
 
 var name: String
+var description: String
 var model: PackedScene
 var effects: Dictionary
 
