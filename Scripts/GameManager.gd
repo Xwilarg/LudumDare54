0e15eb0f87e0e03d70f8b74890bb5ac82e75fd8e
Luxio-Chaosyn <lacendrelame@outlook.fr>
2023-09-30 15:34:51 +0200

    Fix Raycast (no collision mask)

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -24,12 +24,12 @@ func load_item(item: Card) -> void:
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1:
 		var camera3d = get_node("/root/Root/Camera3D")
-		var space_state = get_world_3d().direct_space_state
-		var mousepos = event.position
+		var space_state = get_tree().get_root().get_world_3d().direct_space_state
+		var m_pos = get_viewport().get_mouse_position()
 
-		var from = camera3d.project_ray_origin(event.position)
-		var to = from + camera3d.project_ray_normal(event.position) * 100000.0
+		var from = camera3d.project_ray_origin(m_pos)
+		var to = from + camera3d.project_ray_normal(m_pos) * 100000.0
 		var query = PhysicsRayQueryParameters3D.create(from, to)
 
 		var result = space_state.intersect_ray(query)
-		print(len(result))
+		print(result)
