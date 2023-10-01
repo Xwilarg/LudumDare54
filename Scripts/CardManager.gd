9a0a9710b0321189c817c7eb1539e94b0cfe12b7
Xwilarg <xwilarg@protonmail.com>
2023-10-01 17:23:12 +0200

    Fix names

--- Scripts/CardManager.gd
+++ Scripts/CardManager.gd
@@ -8,7 +8,11 @@ var _items: Array[ItemCard]
 var _energy_max: int = 100
 var _energy_used: int = 0
 
-var catVideo = preload("res://Videos/catVideo1.ogv")
+var catVideo = [
+	preload("res://Videos/catVideo1.ogv")
+]
+
+var animal_video_timer: int
 
 func can_be_placed(card: Card) -> bool:
 	if _energy_used + card.energyCost > _energy_max:
@@ -23,6 +27,12 @@ func get_effect(str: String) -> Array[int]:
 			data.append(int(item.card.effects[str]))
 	
 	return data
+
+func sum(arr: Array[int]) -> int:
+	var val: int = 0
+	for nb in arr:
+		val += nb
+	return val
 
 func _process(delta):
 	_aaTimer -= delta
@@ -39,16 +49,18 @@ func _process(delta):
 		
 		_aaTimer = 1.0
 
+	if animal_video_timer > 0.0:
+		animal_video_timer -= delta
+		if animal_video_timer <= 0.0:
+			play_animal_video()
+
 func register_item(item: ItemCard):
 	_energy_used += item.card.energyCost
 	_items.append(item)
 	if item.card.effects.has("ENG"):
 		_energy_used -= int(item.card.effects["ENG"])
 		update_ui()
-	if item.card.effects.has("VID"):
-		var player: VideoStreamPlayer = get_node("/root/Root/UI/VideoStreamPlayer")
-		player.stream = catVideo
-		player.play()
+	play_animal_video()
 	print("[CM] " + item.card.name + " placed, energy left: " + str(_energy_max - _energy_used))
 
 func delete_item(node: Node3D):
@@ -60,6 +72,13 @@ func delete_item(node: Node3D):
 			_items.erase(item)
 			if len(get_effect("VID")) == 0:
 				get_node("/root/Root/UI/VideoStreamPlayer").stream = null
+
+func play_animal_video():
+	if sum(get_effect("VID")) > 0:
+		var player: VideoStreamPlayer = get_node("/root/Root/UI/VideoStreamPlayer")
+		player.stream = catVideo
+		player.play()
+		animal_video_timer = 20
 
 func update_ui():
 	pass
