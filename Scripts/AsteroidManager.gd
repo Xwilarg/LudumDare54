4f6e01426988c801aaaf05ce9358926c433ca3ea
Luxio-Chaosyn <lacendrelame@outlook.fr>
2023-10-01 12:13:24 +0200

    Add in editor asteroid spawn placing

--- Scripts/AsteroidManager.gd
+++ Scripts/AsteroidManager.gd
@@ -3,11 +3,14 @@ extends Node
 class_name AsteroidManager
 
 const _prefab_spawner = preload("res://Scenes/AsteroidSpawner.tscn")
-const _default_spawns = [Vector3(-10, 0, 10), Vector3(10, 0, 10), Vector3(-10, 0, 0)]  # todo: change to real spawn points
+var _default_spawns = [Vector3(-10, 0, 10), Vector3(10, 0, 10), Vector3(-10, 0, 0)]  # todo: change to real spawn points
 
 const _additional_spawners_timer = 10
 
+@export var _editor_spawns:Array = []
+
 func _ready():
+	_get_spawners();
 	for pos in _default_spawns:
 		new_spawner(pos)
 
@@ -20,3 +23,9 @@ func new_spawner(position = null):
 	
 	var pos_index = GameManager.rng.randi_range(0, len(_default_spawns) - 1)
 
+func _get_spawners():
+	for ndpath in _editor_spawns:
+		var spawn = get_node(ndpath);
+		_default_spawns.push_back(spawn.global_position);
+	print(_editor_spawns);
+	print(_default_spawns);
