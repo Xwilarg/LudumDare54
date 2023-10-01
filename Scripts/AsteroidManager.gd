2c21b57dd76c060ea09d795349d1e9b72dcefdab
Xwilarg <xwilarg@protonmail.com>
2023-10-01 11:55:24 +0200

    Add card manager

--- Scripts/AsteroidManager.gd
+++ Scripts/AsteroidManager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+class_name AsteroidManager
+
 const _prefab_Asteroids = [preload("res://Scenes/AsteroidModel/Asteroid_game1.tscn"), preload("res://Scenes/AsteroidModel/Asteroid_game2.tscn")]
 const _default_spawns = [Vector3(-10, 0, 10), Vector3(10, 0, 10), Vector3(20, 0, 10)]  # todo: change to real spawn points
 
@@ -7,18 +9,10 @@ var _asteroids: Array[Asteroid]
 
 @export var spaceship: Node3D
 
-var _aaTimer: float = 1.0
-
 func _ready():
 	new_asteroid()
 	
 	$SpawnTimer.start()
-
-func _process(delta):
-	_aaTimer -= delta
-	if _aaTimer <= 0.0:
-		_asteroids[GameManager.rng.randi_range(0, len(_asteroids) - 1)].take_damage(10)
-		_aaTimer = 1.0
 
 
 func _on_spawn_timer_timeout():
