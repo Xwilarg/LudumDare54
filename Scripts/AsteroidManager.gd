c182414b0f908de76c7a5eb5d33ad6653454e5a1
François Carré <carre.francois@ymail.com>
2023-10-01 11:54:11 +0200

    Add asteroidspawners (WIP)

--- Scripts/AsteroidManager.gd
+++ Scripts/AsteroidManager.gd
@@ -1,42 +1,29 @@
 extends Node
 
-const _prefab_Asteroids = [preload("res://Scenes/AsteroidModel/Asteroid_game1.tscn"), preload("res://Scenes/AsteroidModel/Asteroid_game2.tscn")]
-const _default_spawns = [Vector3(-10, 0, 10), Vector3(10, 0, 10), Vector3(20, 0, 10)]  # todo: change to real spawn points
+const _prefab_spawner = preload("res://Scenes/AsteroidSpawner.tscn")
+const _default_spawns = [Vector3(-10, 0, 10), Vector3(10, 0, 10), Vector3(-10, 0, 0)]  # todo: change to real spawn points
 
-var _asteroids: Array[Asteroid]
-
-@export var spaceship: Node3D
+const _additional_spawners_timer = 10
 
 var _aaTimer: float = 1.0
 
 func _ready():
-	new_asteroid()
-	
-	$SpawnTimer.start()
+	for pos in _default_spawns:
+		new_spawner(pos)
 
 func _process(delta):
 	_aaTimer -= delta
 	if _aaTimer <= 0.0:
-		_asteroids[GameManager.rng.randi_range(0, len(_asteroids) - 1)].take_damage(10)
+		#_asteroids[GameManager.rng.randi_range(0, len(_asteroids) - 1)].take_damage(10)
 		_aaTimer = 1.0
 
 
 func _on_spawn_timer_timeout():
 	$SpawnTimer.wait_time = GameManager.rng.randi_range(5, 10)
-	new_asteroid()
+
+func new_spawner(position = null):
+	if not position:
+		pass
 	
-func new_asteroid():
-	var index_prefab = GameManager.rng.randi_range(0, 1)
-	
-	var asteroid = _prefab_Asteroids[index_prefab]
-	asteroid = asteroid.instantiate()
-	add_child(asteroid)
-	
-	_asteroids.append(asteroid)
-	
-	var rand_position = func(): return GameManager.rng.randf_range(-3, 3)
-	var start_position = Vector3(rand_position.call(), rand_position.call(), rand_position.call())
-	start_position += _default_spawns[GameManager.rng.randi_range(0, 2)]
-	
-	asteroid.position = spaceship.position + start_position
-	asteroid.set_target(spaceship)
+	var pos_index = GameManager.rng.randi_range(0, len(_default_spawns) - 1)
+
---
+++ Scripts/AsteroidSpawner.gd
@@ -0,0 +1,46 @@
+extends Node
+
+const _prefab_Asteroids = [preload("res://Scenes/AsteroidModel/Asteroid_game1.tscn"), preload("res://Scenes/AsteroidModel/Asteroid_game2.tscn")]
+
+var spawn_position: Vector3
+var _asteroids: Array[Asteroid]
+@onready var spawn_timer: float = 5
+@onready var spawn_random_timer: float = 2
+
+@export var spaceship: Node3D
+
+func set_spawn_position(value: Vector3):
+	spawn_position = value
+
+func increase_spawn_rate():
+	var change_rate = 0.8
+	
+	spawn_timer *= change_rate
+	spawn_random_timer *= change_rate
+
+func _ready():
+	$SpawnTimer.wait_time = spawn_timer
+	$SpawnTimer.start()
+
+func _process(delta):
+	pass
+
+func _on_spawn_timer_timeout():
+	$SpawnTimer.wait_time = spawn_timer + GameManager.rng.randi_range(-spawn_random_timer, spawn_random_timer)
+	new_asteroid()
+	
+func new_asteroid():
+	var index_prefab = GameManager.rng.randi_range(0, 1)
+	
+	var asteroid = _prefab_Asteroids[index_prefab]
+	asteroid = asteroid.instantiate()
+	add_child(asteroid)
+	
+	_asteroids.append(asteroid)
+	
+	var rand_position = func(): return GameManager.rng.randf_range(-3, 3)
+	var start_position_noise = Vector3(rand_position.call(), rand_position.call(), rand_position.call())
+	var start_position = spawn_position + start_position_noise
+	
+	asteroid.position = spaceship.position + start_position
+	asteroid.set_target(spaceship)
