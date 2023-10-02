5f4b9cc74c21c9f429ca1d463941cdf19432e967
François Carré <carre.francois@ymail.com>
2023-10-02 14:02:51 +0200

    Fix launch indivdual scene impossible

--- Scripts/GameManager.gd
+++ Scripts/GameManager.gd
@@ -67,32 +67,33 @@ func _process(delta):
 			hintObject.global_position = result.position
 	
 	# update UI
-	var info_level = CardManager.sum(CardManager.get_effect("SEN"))
-	var text = ""
-	
-	if info_level > 0: # Ship HP
-		var spaceship = get_node("/root/Root/spaceship")
-		text += tr("SHIP_HULL") + tr("COLON") + " " + str(spaceship.current_hp) + "/" + str(spaceship.max_hp) + "\n"
-		text += tr("SHIELD") + tr("COLON") + " " + str(spaceship.current_shield) + "\n\n"
-	
-	if info_level > 1: # Energy
-		var asteroids = get_node("/root/Root/AsteroidManager").get_all_asteroids()
-		text += tr("ENERGY") + tr("COLON") + " " + str(CardManager._energy_max - CardManager._energy_used) + "/" + str(CardManager._energy_max) + "\n\n"
-
-	if info_level > 2: # Attack power
-		text += tr("ATTACK_POWER") + "\n"
-		text += tr("GRN_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_GRN"))) + "\n"
-		text += tr("BLU_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_BLU"))) + "\n"
-		text += tr("RED_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_RED")))
-		text += "\n\n"
-
-	if info_level > 3:
-		text += tr("METEO") + "\n" + meteo + "\n\n"
-
-	if info_level > 4:
-		text += tr("FAVORITE_CHEESE") + "\n" + tr("FETA") + "\n" + tr("GRUYERE") + "\n" + tr("AMERICAN_CHEESE") + "\n\n"
+	if get_node("/root/Root/UI/CaptorInfo"):
+		var info_level = CardManager.sum(CardManager.get_effect("SEN"))
+		var text = ""
 		
-	get_node("/root/Root/UI/CaptorInfo").text = text
+		if info_level > 0: # Ship HP
+			var spaceship = get_node("/root/Root/spaceship")
+			text += tr("SHIP_HULL") + tr("COLON") + " " + str(spaceship.current_hp) + "/" + str(spaceship.max_hp) + "\n"
+			text += tr("SHIELD") + tr("COLON") + " " + str(spaceship.current_shield) + "\n\n"
+		
+		if info_level > 1: # Energy
+			var asteroids = get_node("/root/Root/AsteroidManager").get_all_asteroids()
+			text += tr("ENERGY") + tr("COLON") + " " + str(CardManager._energy_max - CardManager._energy_used) + "/" + str(CardManager._energy_max) + "\n\n"
+
+		if info_level > 2: # Attack power
+			text += tr("ATTACK_POWER") + "\n"
+			text += tr("GRN_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_GRN"))) + "\n"
+			text += tr("BLU_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_BLU"))) + "\n"
+			text += tr("RED_DMG") + tr("COLON") + " " + str(CardManager.sum(CardManager.get_effect("ATK_RED")))
+			text += "\n\n"
+
+		if info_level > 3:
+			text += tr("METEO") + "\n" + meteo + "\n\n"
+
+		if info_level > 4:
+			text += tr("FAVORITE_CHEESE") + "\n" + tr("FETA") + "\n" + tr("GRUYERE") + "\n" + tr("AMERICAN_CHEESE") + "\n\n"
+		
+		get_node("/root/Root/UI/CaptorInfo").text = text
 
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == 1 and _selected_card != null:
