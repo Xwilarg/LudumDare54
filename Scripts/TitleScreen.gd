6ad5bc180b6ed31ce52b7a8a59a0ca70f9511bba
François Carré <carre.francois@ymail.com>
2023-10-02 15:12:55 +0200

    Add (very dirty) language change

--- Scripts/TitleScreen.gd
+++ Scripts/TitleScreen.gd
@@ -1,12 +1,15 @@
 extends Node
-
+var locale = OS.get_locale()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	if locale != "fr_FR":
+		locale = "en"
+	else:
+		locale = "fr"
+	
 	# get correct translation
-	$MarginContainer/VBoxContainer/VBoxContainer/Start.text = tr("MENU_START")
-	$MarginContainer/VBoxContainer/VBoxContainer/Credits.text = tr("MENU_CREDITS")
-	$MarginContainer/VBoxContainer/VBoxContainer/Language.text = tr("MENU_LANGUAGE")
+	_menu_in_language(locale)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,4 +29,17 @@ func _on_credits_gui_input(event):
 
 func _on_language_gui_input(event):
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
-		print("Clicked")
+		if locale == "en":
+			TranslationServer.set_locale("fr")
+			_menu_in_language("fr")
+			locale = "fr"
+		else:
+			TranslationServer.set_locale("en")
+			_menu_in_language("en")
+			locale = "en"
+		
+
+func _menu_in_language(locale: String = "en"):
+		$MarginContainer/VBoxContainer/VBoxContainer/Start.text = tr("MENU_START")
+		$MarginContainer/VBoxContainer/VBoxContainer/Credits.text = tr("MENU_CREDITS")
+		$MarginContainer/VBoxContainer/VBoxContainer/Language.text = tr("MENU_LANGUAGE")
